552444f1c2e18e0681c2c4e87457f08cbe7b1512
elias geiss <e.geiss@division.ag>
2018-06-07 17:52:25 +0200

    [TASK] Fluid integration, removed need for fontawesome, Typoscript integration [ADD] Default style

--- Classes/Controller/IframeRenderingController.php
+++ Classes/Controller/IframeRenderingController.php
@@ -67,12 +67,36 @@ class IframeRenderingController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugi
      * @return string HTML output
      */
     public function wrapIframe($content = '', $conf) {
+        $objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+        $configurationManager = $objectManager->get('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManagerInterface');
+        $typoScriptSettings = $configurationManager->getConfiguration(
+            \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT,
+              'gdpr_youtube',
+              'tx_gdpryoutube'
+            )['plugin.']['tx_gdpryoutube.'];
+
+        $enableReplacement = $typoScriptSettings['settings.']['enableReplacement'];
+        if(!$enableReplacement) return $content;
+
+        $templateRootPaths = $typoScriptSettings['view.']['templateRootPaths.'];
+        $layoutRootPaths = $typoScriptSettings['view.']['layoutRootPaths.'];
+        $partialRootPaths = $typoScriptSettings['view.']['partialRootPaths.'];
+        $templateName = $typoScriptSettings['view.']['templateName'];
+
+        $view = $objectManager->get('TYPO3\\CMS\\Fluid\\View\\StandaloneView');
+        $view->setTemplateRootPaths($templateRootPaths);
+        $view->setLayoutRootPaths($layoutRootPaths);
+        $view->setPartialRootPaths($partialRootPaths);
+        $view->setTemplate($templateName);
+
+        $privacyPageUid = $typoScriptSettings['settings.']['privacyPageUid'];
+        
         $frames = array();
         preg_match_all("/<iframe[^<>]*?>[^<>]*?<\/iframe>/", $content, $frames);
         $res = $content;
         foreach($frames[0] as $iter => $frame) {
             if(strpos($frame, "youtube") !== FALSE){
-                $res = $this->processFrame($frame, $res, $iter);
+                $res = $this->processFrame($view, $frame, $res, $privacyPageUid);
             }
         }
         return $res;
@@ -81,33 +105,23 @@ class IframeRenderingController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugi
     /**
      * Applies necessary changes to the total string based on the frame
      * 
+     * 
      * @param string $frame The individual frame to be wrapped
      * @param string $total The complete output in which to replace the frame with its wrapped counterpart
      * @return string new output with wrapped frames
      */
-    protected function processFrame($frame, $total, $frameId){
-        $datenschutzLink = "/datenschutz.html";
+    protected function processFrame($view, $frame, $total, $privacyPageUid){
+
+        $view->assignMultiple([
+            'data-replace' => $frame,
+            'privacyPageUid' => $privacyPageUid
+        ]);
+        $body = $view->render();
 
         return str_replace(
             $frame,
-            '<div class="youtubeAgreement" data-flag="iframe" '. "data-replace='"
-                . $frame . "'"
-                . 'data-id="' . $frameId . '"'
-            . '>'
-                . '<div class="youtubeAgreementInner">'
-                    . '<div class="youtubeAgreementIcon">'
-                        . '<i class="fa fa-youtube-play" aria-hidden="true"></i>'
-                    . '</div>'
-                    . '<div class="youtubeAgreementText">'
-                        . '<p>'
-                            . 'Das Video wird erst nach dem Klick von Youtube geladen und abgespielt. '
-                            . 'Dazu baut ihr Browser eine direkte Verbindung zu den Youtube-Servern auf. '
-                            . 'Mehr Informationen entnehmen Sie unserer <a class="youtubeAgreementDatenschutzlink" href="'. $datenschutzLink .'">Datenschutzerklärung</a>.'
-                        . '</p>'
-                    . '</div>'
-                . '</div>'
-            . '</div>'
-            ,$total
+            $body,
+            $total
         );
     }
 }
