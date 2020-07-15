cb40783607e584d6801eaf7b782fdecfc794d721
e.geiss <e.geiss@division.ag>
2020-07-15 12:17:16 +0200

    [BUGFIX] pageTS declaration

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:gdpr_youtube/Configuration/TSconfig/pageTSConfig.txt">');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:gdpr_youtube/Configuration/TSconfig/pageTSConfig.tsconfig">');
